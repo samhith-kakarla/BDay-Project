0f73b201014807db96cd9e9b05ab9d91ed7db4d9
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-03-02 00:25:42 -0500

    Created and Configured App Store

--- frontend/store/store.ts
+++ frontend/store/store.ts
@@ -1,2 +1,24 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux'; 
+import thunk, { ThunkMiddleware } from 'redux-thunk'; 
+import { createLogger } from 'redux-logger'; 
 
+// REDUCERS
+import { authReducer } from './reducers/authReducer'; 
+
+// ACTION TYPES
+import { rootActions } from './types/rootActionTypes'; 
+
+
+const logger = createLogger(); 
+
+export const rootReducer = combineReducers({
+    authReducer,
+}); 
+
+export type AppState = ReturnType<typeof rootReducer>; 
+
+// STORE
+export const store = createStore<AppState, rootActions, {}, {}>(
+    rootReducer, 
+    applyMiddleware(thunk as ThunkMiddleware<AppState, rootActions>, logger), 
+); 
