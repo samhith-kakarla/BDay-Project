c568a4789f37befa207e9879b2c313a2be43d9fb
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-03-15 15:15:55 -0400

    Created Auth Context Actions

--- frontend/context/authContext.tsx
+++ frontend/context/authContext.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'; 
+import axios from 'axios'; 
 
 // TYPES
 import { User } from './types'; 
@@ -49,44 +50,297 @@ const AuthContextProvider: React.FC = ({ children }) => {
     const [refresh, setRefresh] = React.useState<string>(authContextDefault.refresh); 
     const [isAuthenticated, setIsAuthenticated] = React.useState<boolean>(authContextDefault.isAuthenticated);
 
-    const checkAuthenticated = () => {
+    const checkAuthenticated = () => async () => {
+        if (typeof window == "undefined") {
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        } else {
+            if (localStorage.getItem("access")) {
+                const config = {
+                    headers: {
+                        'Accept': 'application/json', 
+                        'Content-type': 'application/json', 
+                    }
+                }
         
+                const body = JSON.stringify({ token: localStorage.getItem("access") }); 
+        
+                try {
+                    const res = await axios.post('http://127.0.0.1:8000/api/auth/jwt/verify/', body, config); 
+                    if (res.data.code !== 'token_not_valid') {
+                        setUser(null); 
+                        setAccess(""); 
+                        setRefresh(""); 
+                        setIsAuthenticated(true); 
+                    } else {
+                        setUser(null); 
+                        setAccess(""); 
+                        setRefresh(""); 
+                        setIsAuthenticated(false); 
+                    }
+                } catch (error) {
+                    console.log(error); 
+                    setUser(null); 
+                    setAccess(""); 
+                    setRefresh(""); 
+                    setIsAuthenticated(false); 
+                }
+            } else {
+                setUser(null); 
+                setAccess(""); 
+                setRefresh(""); 
+                setIsAuthenticated(false); 
+            }
+        }
     }
 
-    const loadUser = () => {
+    const loadUser = () => async() => {
+        if (typeof window !== "undefined") {
+            if (localStorage.getItem("access")) {
+                const config = {
+                    headers: {
+                        'Accept': 'application/json', 
+                        'Authorization': `JWT ${localStorage.getItem("access")}`, 
+                        'Content-type': 'application/json', 
+                    }
+                }; 
 
+                try {
+                    const res = await axios.get('http://127.0.0.1:8000/api/auth/users/me/', config); 
+                    
+                    setUser(res.data); 
+                    setAccess(""); 
+                    setRefresh(""); 
+                    setIsAuthenticated(null);   
+                } catch (error) {
+                    console.log(error); 
+                    setUser(null); 
+                    setAccess(""); 
+                    setRefresh(""); 
+                    setIsAuthenticated(null); 
+                }
+            } else {
+                setUser(null); 
+                setAccess(""); 
+                setRefresh(""); 
+                setIsAuthenticated(null); 
+            }
+        }
     }
 
-    const googleAuthenticate = () => {
+    const googleAuthenticate = (state: string, code: string) => async () => {
+        if (typeof window !== "undefined") {
+            if (state && code && !localStorage.getItem("access")) {
+                const config = {
+                    headers: {
+                        'Content-type': 'application/x-www-form-urlencoded', 
+                    }
+                }; 
         
+                const details = {
+                    'state': state, 
+                    'code': code,
+                }; 
+        
+                const formBody = Object.keys(details).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(details[key])).join('&');
+        
+                try {
+                    const res = await axios.post(`http://127.0.0.1:8000/api/auth/o/google-oauth2/?${formBody}`, config); 
+        
+                    setUser(null); 
+                    setAccess(res.data.access); 
+                    setRefresh(res.data.refresh); 
+                    setIsAuthenticated(true); 
+        
+                    loadUser(); 
+                } catch (error) {
+                    console.log(error); 
+                    
+                    setUser(null); 
+                    setAccess(""); 
+                    setRefresh(""); 
+                    setIsAuthenticated(false); 
+                }
+            }
+        }
     }
 
-    const facebookAuthenticate = () => {
+    const facebookAuthenticate = (state: string, code: string) => async () => {
+        if (typeof window !== "undefined") {
+            if (state && code && !localStorage.getItem("access")) {
+                const config = {
+                    headers: {
+                        'Content-type': 'application/x-www-form-urlencoded', 
+                    }
+                }; 
         
+                const details = {
+                    'state': state, 
+                    'code': code,
+                }; 
+        
+                const formBody = Object.keys(details).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(details[key])).join('&');
+        
+                try {
+                    const res = await axios.post(`http://127.0.0.1:8000/api/auth/o/facebook/?${formBody}`, config); 
+        
+                    setUser(null); 
+                    setAccess(res.data.access); 
+                    setRefresh(res.data.refresh); 
+                    setIsAuthenticated(true); 
+        
+                    loadUser(); 
+                } catch (error) {
+                    console.log(error); 
+                    
+                    setUser(null); 
+                    setAccess(""); 
+                    setRefresh(""); 
+                    setIsAuthenticated(false); 
+                }
+            }
+        }
     }
 
-    const login = () => {
-        
+    const login = (email: string, password: string) => async () => {
+        const config = {
+            headers: {
+                'Content-type': 'application/json', 
+            }
+        }; 
+    
+        const body = JSON.stringify({ email, password }); 
+    
+        try {
+            const res = await axios.post('http://127.0.0.1:8000/api/auth/jwt/create/', body, config); 
+    
+            setUser(null); 
+            setAccess(res.data.access); 
+            setRefresh(res.data.refresh); 
+            setIsAuthenticated(true); 
+    
+            loadUser(); 
+        } catch (error) {
+            console.log(error); 
+    
+            setUser(null); 
+            setAccess(null); 
+            setRefresh(null); 
+            setIsAuthenticated(false);  
+        }
     }
 
-    const signup = () => {
-        
+    const signup = (
+        firstName: string, lastName: string, email: string, password: string, re_password: string
+    ) => async () => {
+        const config = {
+            headers: {
+                'Content-type': 'application/json', 
+            }
+        }; 
+    
+        const body = JSON.stringify({ firstName, lastName, email, password, re_password }); 
+    
+        try {
+            await axios.post('http://127.0.0.1:8000/api/auth/users/', body, config); 
+    
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        } catch (error) {
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        }
     }
 
-    const logout = () => {
-        
+    const logout = () => async () => {
+        setUser(null); 
+        setAccess(""); 
+        setRefresh(""); 
+        setIsAuthenticated(false); 
     }
 
-    const verify = () => {
-        
+    const verify = (uid: string, token: string) => async () => {
+        const config = {
+            headers: {
+                'Content-type': 'application/json', 
+            }
+        }; 
+    
+        const body = JSON.stringify({ uid, token }); 
+    
+        try {
+            await axios.post('http://127.0.0.1:8000/api/auth/users/activation/', body, config); 
+    
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        } catch (error) {
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        }
     }
 
-    const resetPassword = () => {
-        
+    const resetPassword = (email: string) => async () => {
+        const config = {
+            headers: {
+                'Content-type': 'application/json', 
+            }
+        }; 
+    
+        const body = JSON.stringify({ email }); 
+    
+        try {
+            await axios.post('http://127.0.0.1:8000/api/auth/users/reset_password/', body, config); 
+            
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        } catch (error) {
+            console.log(error); 
+    
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        }
     }
 
-    const resetPasswordConfirm = () => {
-        
+    const resetPasswordConfirm = (
+        uid: string, token: string, new_password: string, re_new_password: string
+    ) => async () => {
+        const config = {
+            headers: {
+                'Content-type': 'application/json', 
+            }
+        }; 
+    
+        const body = JSON.stringify({ uid, token, new_password, re_new_password }); 
+    
+        try {
+            await axios.post('http://127.0.0.1:8000/api/auth/users/reset_password_confirm/', body, config); 
+            
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        } catch (error) {
+            console.log(error); 
+    
+            setUser(null); 
+            setAccess(""); 
+            setRefresh(""); 
+            setIsAuthenticated(false); 
+        }
     }
 
     return (
