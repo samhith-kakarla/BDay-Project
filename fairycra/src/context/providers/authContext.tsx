9bd1f2b2fd40ad3335b58e15b4fd9560597ca203
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-04-28 19:48:00 -0400

    Created Firebase Auth Actions

--- fairycra/src/context/providers/authContext.tsx
+++ fairycra/src/context/providers/authContext.tsx
@@ -11,12 +11,10 @@ export type AuthContextType = {
     // ACTIONS
     googleAuthenticate: () => void;
     login: (email: string, password: string) => void;
-    signup: (
-        fName: string, lName: string, email: string, pass: string, re_pass: string
-    ) => void;
+    signup: (fName: string, lName: string, email: string, pass: string) => void;
     logout: () => void;
-    resetPassword: (email: string) => void;
-    resetPasswordConfirm: (uid: string, token: string, new_password: string, re_new_password: string) => void;
+    sendResetPasswordLink: (email: string) => void;
+    resetPassword: (new_pass: string) => void;
 }
 
 const authContextDefault: AuthContextType = {
@@ -30,8 +28,8 @@ const authContextDefault: AuthContextType = {
     login: () => {}, 
     logout: () => {}, 
     signup: () => {}, 
+    sendResetPasswordLink: () => {}, 
     resetPassword: () => {}, 
-    resetPasswordConfirm: () => {}, 
 }
 
 export const AuthContext = React.createContext<AuthContextType>(authContextDefault); 
@@ -51,7 +49,6 @@ const AuthContextProvider: FC = ({ children }) => {
     function login (email: string, password: string) {
         firebase.auth().signInWithEmailAndPassword(email, password).then((response) => {
             console.log(response); 
-            setUser(response); 
             setIsAuthenticated(true); 
         }).catch((error) => {
             console.log(error); 
@@ -59,30 +56,89 @@ const AuthContextProvider: FC = ({ children }) => {
         })
     }
 
-    function signup (fName: string, lName: string, email: string, pass: string, re_pass: string) {
-
+    function signup (fName: string, lName: string, email: string, pass: string) {
+        firebase.auth().createUserWithEmailAndPassword(email, pass).then(
+            (cred: firebase.auth.UserCredential) => {
+                // Add First Name and Last Name to DB
+                firebase.firestore().collection('users').doc(cred.user!.uid).set({
+                    email: email, 
+                    firstName: fName, 
+                    lastName: lName, 
+                }).then(() => {
+                    console.log("User Created!"); 
+                    setIsAuthenticated(true);
+                }).catch((error) => {
+                    console.log(error.message); 
+                    setIsAuthenticated(false); 
+                }); 
+            }   
+        ); 
     }
 
     function logout () {
-
+        firebase.auth().signOut().then((res) => {
+            console.log(res); 
+            console.log("User Signed Out"); 
+            setIsAuthenticated(false); 
+        }).catch((error) => {
+            console.log(error); 
+            console.log("Error Signing Out"); 
+            setIsAuthenticated(true); 
+        }); 
     }
 
     function googleAuthenticate () {
+        const provider = new firebase.auth.GoogleAuthProvider(); 
+        firebase.auth().signInWithPopup(provider).then((res) => {
+            const cred: firebase.auth.OAuthCredential = res.credential!; 
+            const accessToken = cred.accessToken;
+            
+            firebase.firestore().collection('users').doc(res.user?.uid).set({
+                email: res.user?.email, 
+                name: res.user?.displayName, 
+            }).then(() => {
+                console.log("User created!"); 
+            }).catch((error) => {
+                console.log(error); 
+                console.log("User not created :("); 
+            }); 
 
+            console.log("Google Sign-In Success"); 
+            setIsAuthenticated(true);  
+        }).catch((error) => {
+            console.log(error.message); 
+            setIsAuthenticated(false); 
+        });
     }
 
-    function resetPassword (email: string) {
-        
+    function sendResetPasswordLink (email: string) {
+        firebase.auth().sendPasswordResetEmail(email).then(() => {
+            console.log("Reset Password Link Sent!"); 
+            setIsAuthenticated(false); 
+        }).catch((error) => {
+            console.log(error); 
+            console.log("Reset Password Link not Sent"); 
+            setIsAuthenticated(false); 
+        }); 
     }
 
-    function resetPasswordConfirm (uid: string, new_pass: string, re_new_pass: string) {
-        
+    function resetPassword (new_pass: string) {
+        const user = firebase.auth().currentUser; 
+
+        user?.updatePassword(new_pass).then(() => {
+            console.log("Password Updated!"); 
+            setIsAuthenticated(false); 
+        }).catch((error) => {
+            console.log(error); 
+            console.log("Password not reset"); 
+            setIsAuthenticated(false); 
+        }); 
     }
 
     return (
         <AuthContext.Provider value={{ 
             user, isAuthenticated, loadingAuthState, 
-            googleAuthenticate, login, signup, logout, resetPassword, resetPasswordConfirm
+            googleAuthenticate, login, signup, logout, sendResetPasswordLink, resetPassword
         }}>
             {children}
         </AuthContext.Provider>
