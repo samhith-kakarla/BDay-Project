4c0ceac420b94c85afe9b5da71603597fa45d90d
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-04-29 02:36:52 -0400

    Setup Firebase Twin Context Actions

--- fairycra/src/context/providers/twinContext.tsx
+++ fairycra/src/context/providers/twinContext.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState, createContext } from 'react';
+import firebase from '../../firebase/firebaseConfig'; 
 
 // TYPES
 import { Twin } from '../types'; 
@@ -10,7 +11,12 @@ export type TwinContextType = {
     currentTwin: Twin | null;
     
     // ACTIONS
-    getMyTwins: (ownerID: string) => void;
+    getMyTwins: (ownerID: string) => void; // Firestore
+    addNewTwin: (twin: Twin) => void; // Firestore
+    updateTwinInfo: (twin: Twin) => void; // Firestore
+    deleteTwin: (twin: Twin) => void; // Firestore
+    updateTwinImages: (twin: Twin) => void; // Storage
+    getTwinImages: (twin: Twin) => void; // Storage
 }
 
 const twinContextDefault: TwinContextType = {
@@ -20,6 +26,11 @@ const twinContextDefault: TwinContextType = {
 
     // ACTIONS
     getMyTwins: () => {}, 
+    addNewTwin: () => {},
+    updateTwinInfo: () => {}, 
+    deleteTwin: () => {},
+    updateTwinImages: () => {}, 
+    getTwinImages: () => {},
 }
 
 
@@ -30,13 +41,55 @@ const TwinContextProvider: FC = ({ children }) => {
     const [currentTwin, setCurrentTwin] = useState(twinContextDefault.currentTwin);
 
     function getMyTwins (owner: string) {
-        
+        firebase.firestore().collection('twins').where("owner", "==", owner).get().then((query) => {
+            console.log(query); 
+            query.docs.forEach((doc) => {
+                const myTwin: Twin = {
+                    id: doc.id, 
+                    name: doc.data().name, 
+                    age: doc.data().age, 
+                    birthday: doc.data().birthday,
+                    address: doc.data().address, 
+                    cake_tags: doc.data().cake_tags,
+                    match: doc.data().match, 
+                }; 
+                setTwins([...twins, myTwin]); 
+            }); 
+        }).then(() => {
+            console.log(twins); 
+            console.log("My Twins receieved!"); 
+        }).catch((error) => {
+            console.log(error); 
+            console.log("Twins not received :("); 
+            setTwins([]); 
+        });
+    }
+
+    function addNewTwin (twin: Twin) {
+
+    }
+
+    function updateTwinInfo (twin: Twin) {
+
+    }
+
+    function deleteTwin (twin: Twin) {
+
+    }
+
+    function updateTwinImages (twin: Twin) {
+
+    }
+
+    function getTwinImages (twin: Twin) {
+
     }
 
     return (
         <TwinContext.Provider value={{ 
             twins, currentTwin, 
-            getMyTwins, 
+            getMyTwins, addNewTwin, updateTwinInfo, deleteTwin, 
+            updateTwinImages, getTwinImages
         }}>
             { children }
         </TwinContext.Provider>
