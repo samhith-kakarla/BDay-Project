03b52bcf98f680a4dbfa1828aab25245643365ad
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-05-03 11:49:02 -0400

    Setup Router

--- fairycra/src/App.tsx
+++ fairycra/src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom'; 
+
+// PAGES
 
 // CONTEXT
 import AppContextProvider from './context/appContext'; 
 
 function App() {
-  return (
-    <AppContextProvider>
-        <div className="App">
-          
-        </div>
-    </AppContextProvider>
-  );
+    return (
+        <AppContextProvider>
+              <Router>
+                  <div className="App">
+                        <Switch>
+                            <Route exact path="/" />
+                        </Switch>
+                  </div>
+              </Router>
+        </AppContextProvider>
+    );
 }
 
 export default App;
