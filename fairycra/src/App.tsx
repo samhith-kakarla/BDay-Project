5193b22cfb89e6736fde21af3997402b2d787b32
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-05-03 23:07:03 -0400

    Setup Cake Provider Routes

--- fairycra/src/App.tsx
+++ fairycra/src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom'; 
 
 // PAGES
+import EnterCred from './pages/cakeProv/EnterCred'; 
+import CakesDashboard from './pages/cakeProv/CakesDashboard'; 
+import CakeOrderInfo from './pages/cakeProv/CakeOrderInfo'; 
 
 // CONTEXT
 import AppContextProvider from './context/appContext'; 
@@ -13,7 +16,18 @@ function App() {
               <Router>
                   <div className="App">
                         <Switch>
+                            {/* MAIN ROUTES */}
                             <Route exact path="/" />
+
+                            {/* FAIRY ROUTES */}
+                                
+                            {/* TWIN PROVIDER ROUTES */}
+                            
+                            {/* CAKE PROVIDER ROUTES */}
+                            <Route exact path={"/c/enter"} component={EnterCred} />
+                            <Route exact path={"/c/dashboard"} component={CakesDashboard} />
+                            <Route exact path={"/c/orderInfo"} component={CakeOrderInfo} />
+                            
                         </Switch>
                   </div>
               </Router>
