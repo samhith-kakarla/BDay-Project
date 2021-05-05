91fc96e43f4c2fcffe5ba73afa0b187a05aec22c
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-05-04 23:46:35 -0400

    Setup Main Pages

---
+++ fairycra/src/pages/main/OurMission.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OurMission: React.FC = () => {
+    return (
+        <div>
+            <h1>OUR MISSION</h1>
+        </div>
+    )
+}
+
+
+export default OurMission; 
