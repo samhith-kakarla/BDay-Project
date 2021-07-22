fdbe99e41023ad61db5ce893802ccf8ecc190643
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-07-22 16:30:57 -0400

    setup pages and routing

--- fairycra-js/src/App.js
+++ fairycra-js/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+// PAGES
+import { Home, OurMission } from './pages/Main';
+import { Begin, SelectTwin, SelectCake, OrderCake, Complete } from './pages/Fairies';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          {/* MAIN ROUTES */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={OurMission} />
+
+          {/* FAIRY ROUTES */}
+          <Route exact path="/" component={Begin} />
+          <Route exact path="/" component={SelectTwin} />
+          <Route exact path="/" component={SelectCake} />
+          <Route exact path="/" component={OrderCake} />
+          <Route exact path="/" component={Complete} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
